6c65f5ff148ef97b38736ea82ddf1108979b75e9
Anastasiia <gorgona1597531597@gmail.com>
2025-12-14 19:43:20 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import styles from './page.module.css';
 
 
 export default function Home() {
-  return (<main className={styles.main}>
+  return (
     <div className={styles.container}>
       {/* hero */}
       {/* <BenefitsBlock /> */}
       {/* tools */}
       {/* feedback */}
       {/* registration block */}
-    </div>
-  </main>);
+    </div>);
 }

7c7ed50b6ae7089ca9a4c468f614a1f9d20c4005
Anastasiia <gorgona1597531597@gmail.com>
2025-12-14 17:20:04 +0100

    update main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (<main className={styles.main}>
-    <h1 className={styles.title}>Ласкаво просимо</h1>
-    <p className={styles.description}>
-      This is the main landing page of the application.
-    </p>
+    {/* hero */}
+    {/* benefits block */}
+    {/* tools */}
+    {/* feedback */}
+    {/* registration block */}
   </main>);
 }

8b50da0f3781e5bb16b01b0231a7e0e54a04adc1
Beowulf3 <vk3volkov@gmail.com>
2025-12-15 14:35:47 +0200

    add components

--- app/(site)/tools/[toolId]/ToolDetails.client.tsx
+++ app/(site)/tools/[toolId]/ToolDetails.client.tsx
@@ -3,53 +3,43 @@ import { useParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 
 import css from "./ToolDetails.module.css";
-import Image from "next/image";
+import ToolInfoBlock from "@/components/tools/ToolInfoBlock";
+// import ToolGallery from "@/components/tools/ToolGallery";
 
 const ToolDetailsClient = () => {
   const { toolId } = useParams<{ toolId: string }>();
 
-  const { data, isLoading, error } = useQuery({
+  const {
+    data: toolData,
+    isLoading: isLoadingTool,
+    error: toolError,
+  } = useQuery({
     queryKey: ["tool", toolId],
     // queryFn: () => getSingleTool(toolId), <-- get function
     refetchOnMount: false,
   });
+  const {
+    data: userData,
+    isLoading: isLoadingUser,
+    error: userError,
+  } = useQuery({
+    queryKey: ["owner" /**toolData?.owner*/],
+    // queryFn: () => getUserById(toolData.owner),
+    enabled: !!toolData,
+    refetchOnMount: false,
+  });
 
-  if (isLoading) return <p>Loading, please wait...</p>;
-  if (error || !data) return <p>Something went wrong.</p>;
+  if (isLoadingTool || isLoadingUser) return <p>Loading, please wait...</p>;
+  if (toolError || userError || !toolData || !userData)
+    return <p>Something went wrong.</p>;
 
   return (
-    data && (
+    toolData &&
+    userData && (
       <div className={css.container}>
         <div className={css.tool_details_wrap}>
-          <Image
-            src="#"
-            width={640}
-            height={480}
-            loading="lazy"
-            alt="Tool picture"
-          />
-          <div className={css.tool_details_content}>
-            <h1 className={css.tool_details_heading}>
-              Дриль алмазного свердління
-            </h1>
-            <p className={css.tool_price}>Price</p>
-            <div className={css.user_profile}>
-              <Image
-                src="#"
-                width={80}
-                height={80}
-                loading="lazy"
-                alt="User avatar"
-              />
-              <h2 className={css.username}>Username</h2>
-              <button className={css.user_profile_btn}>
-                Переглянути профіль
-              </button>
-            </div>
-            <p className={css.tool_description}>About tool</p>
-            <div className={css.tool_specs}>specs array</div>
-            <button className={css.booking_btn}>Забронювати</button>
-          </div>
+          {/* <ToolGallery {data.images} /> */}
+          <ToolInfoBlock tool={toolData} user={userData} />
         </div>
       </div>
     )
---
+++ components/tools/ToolInfoBlock.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+
+import { Tool } from "@/models/Tool";
+import css from "../../app/(site)/tools/[toolId]/ToolDetails.module.css";
+import { User } from "@/models/User";
+interface ToolInfoBlockProps {
+  tool: Pick<Tool, "name" | "pricePerDay" | "description" | "specifications">;
+  user: User;
+}
+const ToolInfoBlock = ({ tool, user }: ToolInfoBlockProps) => {
+  return (
+    <div className={css.tool_details_content}>
+      <h1 className={css.tool_details_heading}>{tool.name}</h1>
+      <p className={css.tool_price}>{tool.pricePerDay}</p>
+      <div className={css.user_profile}>
+        <Image
+          src={user.avatar}
+          width={80}
+          height={80}
+          loading="lazy"
+          alt="User avatar"
+        />
+        <h2 className={css.username}>{user.name}</h2>
+        <button className={css.user_profile_btn}>Переглянути профіль</button>
+      </div>
+      <p className={css.tool_description}>{tool.description}</p>
+      <div className={css.tool_specs}>
+        <ul className={css.tool_spec_list}>
+          {Object.entries(tool.specifications).map(([key, value]) => (
+            <li key={key} className={css.tool_spec_item}>
+              <span className={css.span}>{key}:</span> {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className={css.booking_btn}>Забронювати</button>
+    </div>
+  );
+};
+
+export default ToolInfoBlock;

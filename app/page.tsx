39285cd7c899d51a6155d0033c179f7fb1d7bb0a
Oleksandr Haponov <olex.haponov@gmail.com>
2025-12-16 18:32:47 +0200

    added feature HeroBlock

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import BenefitsBlock from "@/components/home/BenefitsBlock/BenefitsBlock";
 import FeedbacksBlock from "@/components/home/Feedbacks/FeedbacksBlock";
 import RegistrationBlock from "@/components/home/RegistrationBlock";
+import HeroBlock from "@/components/home/HeroBlock/HeroBlock";
 import styles from './page.module.css';
 
 
 export default function Home() {
   return (
     <>
+      <HeroBlock />
       <div className={styles.container}>
-        {/* hero */}
         <BenefitsBlock />
         {/* tools */}
         <FeedbacksBlock />

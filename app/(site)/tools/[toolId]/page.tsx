2f6e422d7cab5ca688fcf4d893ce698487f602e3
Beowulf3 <vk3volkov@gmail.com>
2025-12-17 21:50:23 +0200

    minimal component test

--- app/(site)/tools/[toolId]/page.tsx
+++ app/(site)/tools/[toolId]/page.tsx
@@ -1,25 +1,26 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
+// import {
+//   dehydrate,
+//   HydrationBoundary,
+//   QueryClient,
+// } from "@tanstack/react-query";
 
 type Props = {
-  params: Promise<{ toolId: string }>;
+  params: { toolId: string };
 };
 
 const ToolDetails = async ({ params }: Props) => {
-  const { toolId } = await params;
-  const queryClient = new QueryClient();
+  //const { toolId } = await params;
+  //   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ["tool", toolId],
-    // queryFn: () => getSingleTool(toolId), <-- get function
-  });
+  //   await queryClient.prefetchQuery({
+  //     queryKey: ["tool", toolId],
+  //     // queryFn: () => getSingleTool(toolId), <-- get function
+  //   });
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>Tool</HydrationBoundary>
-  );
+  //   return (
+  //     <HydrationBoundary state={dehydrate(queryClient)}>Tool</HydrationBoundary>
+  //   );
+  return <div>Tool ID: {params.toolId}</div>;
 };
 
 export default ToolDetails;

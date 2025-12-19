e8b286e198141785380aa77f9e4662b0e6352076
Beowulf3 <vk3volkov@gmail.com>
2025-12-19 21:59:04 +0200

    add desctop styles for tool details  page; fix imports in clientApi.ts

--- components/modals/AuthRedirect/AuthRedirectModal.tsx
+++ components/modals/AuthRedirect/AuthRedirectModal.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
 
+import css from "./AuthRedirectModal.module.css";
+
 const AuthRedirectModal = () => {
   return (
-    <div>
-      <div>
-        <h1>Спочатку авторизуйтесь</h1>
-        <p>
+    <div className={css.modalWrap}>
+      <div className={css.headingWrap}>
+        <h1 className={css.heading}>Спочатку авторизуйтесь</h1>
+        <p className={css.text}>
           Щоб забрронювати інструмент, треба спочатку зареєструватись, або
           авторизуватись на платформі
         </p>
       </div>
-      <div>
-        <Link href={"/auth/login"}>Вхід</Link>
-        <Link href={"/auth/register"}>Реєстрація</Link>
+      <div className={css.buttons}>
+        <Link className={css.login} href={"/auth/login"}>
+          Вхід
+        </Link>
+        <Link className={css.register} href={"/auth/register"}>
+          Реєстрація
+        </Link>
       </div>
     </div>
   );
--- components/tools/ToolGallery.tsx
+++ components/tools/ToolGallery.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
+import css from "./ToolGallery.module.css";
+
 type ToolProps = {
   images: string;
 };
 const ToolGallery = ({ images }: ToolProps) => {
   return (
     <Image
+      className={css.toolImage}
       src={images}
       width={640}
       height={480}

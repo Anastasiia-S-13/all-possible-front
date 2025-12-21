c131235ca0aa5fb6b2023de3d2afddda589fdb2e
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-12-21 20:34:42 +0100

    feat: Implement tool booking page and client component, and allow Unsplash images in next.config.

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,12 @@ const nextConfig: NextConfig = {
       protocol: "https",
       hostname: "res.cloudinary.com",
       pathname: "/**",
+    }, {
+      protocol: "https",
+      hostname: "images.unsplash.com",
+      pathname: "/**",
     },
+
 
     { protocol: 'https', hostname: 'ftp.goit.study', pathname: '/**' },
     { protocol: 'https', hostname: 'ac.goit.global', pathname: '/**' }
